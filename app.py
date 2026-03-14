5ef74927f8d0ac25655fdebc2b81d8426a538541
Luciano Oliveira <lucianocosta21@hotmail.com>
2026-03-14 16:39:55 -0300

    refactor: deleta arquivos não utilizados

--- app.py
+++
@@ -1,8 +0,0 @@
-from graph import build_graph
-
-app = build_graph()
-
-resultado = app.invoke({"pergunta": "O que é LangChain"})
-
-print("resposta", resultado["resposta"])
-print("fonte:", resultado["docs"][0].metadata.get("source"))

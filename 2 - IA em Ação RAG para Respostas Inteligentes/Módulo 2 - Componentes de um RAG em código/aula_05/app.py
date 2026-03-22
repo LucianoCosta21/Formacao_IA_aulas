d3ac666c9763ce99e19b5e2f8632164ba47a6fa7
Luciano Oliveira <lucianocosta21@hotmail.com>
2026-03-21 22:12:41 -0300

    Busca simples no banco vetorial

---
+++ 2 - IA em Ação RAG para Respostas Inteligentes/Módulo 2 - Componentes de um RAG em código/aula_05/app.py
@@ -0,0 +1,29 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+import os
+
+
+persist_directory="./chroma_wiki"
+print(os.path.exists(persist_directory))
+
+embeddings = HuggingFaceEmbeddings(
+    model_name="mixedbread-ai/mxbai-embed-large-v1"
+    )
+
+vectordb = Chroma(
+    persist_directory=persist_directory,
+    embedding_function=embeddings,
+    collection_name="wikipedia_pt_rag"  
+)
+
+##Busca simples
+retriever = vectordb.as_retriever(search_kwargs={"k": 3})
+
+query = "Quais foram os principais marcos da Inteligência Artificial?"
+
+resultado = retriever.invoke(query)
+print(resultado)
+for doc in resultado:
+    print(doc.page_content[:200])
+    print(doc.metadata)
+    print("-" * 50)
